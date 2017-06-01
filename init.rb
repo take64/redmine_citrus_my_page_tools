a2b958ecab35f7794536d7ea92774d9b84ec9b02
kouhei.takemoto <take64@citrus.tk>
2017-06-01 22:06:01 +0900

    init

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_citrus_my_page_tools do
+  name 'Redmine Citrus My Page Tools plugin'
+  author 'take64'
+  description 'This is a plugin for Redmine My Page'
+  version '0.0.1'
+  url 'http://github.com/take64'
+  author_url 'http://besidesplus.net/'
+end
