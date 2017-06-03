cceb0f368dd349a22131e39e68ca3ef55535d080
kouhei.takemoto <take64@citrus.tk>
2017-06-04 04:19:17 +0900

    issue box minimize to make it smaller

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_citrus_my_page_tools do
   name 'Redmine Citrus My Page Tools plugin'
   author 'take64'
   description 'This is a plugin for Redmine My Page'
-  version '0.0.1'
+  version '0.0.0.2'
   url 'http://github.com/take64'
   author_url 'http://besidesplus.net/'
 end
