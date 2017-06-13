1722c6a9e579343ca1cff38052aa23fa18b4f7e4
kouhei.takemoto <take64@citrus.tk>
2017-06-14 02:26:15 +0900

    improve task are left

--- app/helpers/groove_calendar_helper.rb
+++ app/helpers/groove_calendar_helper.rb
@@ -85,10 +85,21 @@ module GrooveCalendarHelper
     }
   end
   
-  # find calendar
-  def find_calendar(calendars, day)
-    calendar = calendars.find { |item| (item.startdt <= day && item.enddt >= day) }
-    calendar
+  # task rendering
+  def render_groove_task_progress(issues, day)
+    count_remaining = count_task_remaining(issues)
+    count_completed = count_task_completed(issues)
+    cwday = day.cwday
+    # render tasks
+    if (count_remaining == 0 && count_completed == 0)
+      render_tasks_none
+    elsif (count_remaining == 0 && count_completed != 0)
+      render_tasks_all_completed(cwday)
+    elsif (count_remaining > 0 && count_completed == 0)
+      render_tasks_not_active(cwday)
+    elsif (count_remaining > 0)
+      render_tasks_are_left(count_remaining)
+    end
   end
   
   # css method ratio
@@ -115,5 +126,61 @@ module GrooveCalendarHelper
     ret = 0
     ret = value if value
     ret
+  end
+  
+  private
+  
+  # find calendar
+  def find_calendar(calendars, day)
+    calendar = calendars.find { |item| (item.startdt <= day && item.enddt >= day) }
+    calendar
+  end
+  
+  # count remaining task
+  def count_task_remaining(issues)
+    count = 0
+    issues.each do |issue|
+      # filter
+      next unless issue.is_a?(Issue)
+      # logic
+      count += 1 unless issue.closed?
+    end
+    count
+  end
+  
+  # count completed task
+  def count_task_completed(issues)
+    count = 0
+    issues.each do |issue|
+      # filter
+      next unless issue.is_a?(Issue)
+      # logic
+      count += 1 if issue.closed?
+    end
+    count
+  end
+  
+  # render none tasks
+  def render_tasks_none
+    "task schedule none.".html_safe
+  end
+  
+  # render all completed tasks
+  def render_tasks_all_completed(cwday)
+    color = "#6666CC" if cwday <= 5
+    color = "#FFFFFF" if cwday >= 6
+    "<span style='font-weight:bold;color:#{color};'>task All Completed.</span>".html_safe
+  end
+  
+  # render not active tasks
+  def render_tasks_not_active(cwday)
+    color = "#CC6666" if cwday <= 5
+    color = "#FFFFFF" if cwday >= 6
+    "<span style='font-weight:bold;color:#{color};'>task Not Active...</span>".html_safe
+  end
+  
+  # render are left tasks
+  def render_tasks_are_left(count_remaining)
+    "<span style='font-weight:bold;'>task are left. ( #{count_remaining} )</span>".html_safe
   end
 end
